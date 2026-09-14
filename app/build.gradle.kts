dfbc1a3bea9ebae02dc8232d8a7f21e397ca0ab4
phreakazone <phreakazone@gmail.com>
2026-09-14 15:54:06 +0700

    fix(ci): align Fragment ActivityResult dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
   implementation(platform(libs.firebase.bom))
   // implementation(libs.accompanist.permissions)
   implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.fragment.ktx)
   // implementation(libs.androidx.camera.camera2)
   // implementation(libs.androidx.camera.core)
   // implementation(libs.androidx.camera.lifecycle)
