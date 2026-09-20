2dd868ceca43a0bf328d5c1f645326688b9d13b2
phreakazone <phreakazone@gmail.com>
2026-09-20 11:07:27 +0700

    hardware: complete protection circuits in generate_easyeda.mjs

--- hardware/easyeda/generate_easyeda.mjs
+++ hardware/easyeda/generate_easyeda.mjs
@@ -23,26 +23,27 @@ const espRight = [
 ];
 
 const components = [
-  {ref:'J1',value:'NS200 HARNESS HEADER 2x6 2.54mm',kind:'header2x6',x:4,y:98,pins:[
+  {ref:'J1',value:'NS200 HARNESS HEADER 2x6 2.54mm',kind:'header2x6',x:4,y:145,pins:[
     ['1','NC'],['2','TPS_A'],['3','TEMP_SENSOR'],['4','TPS_B'],['5','IGN_12V'],['6','COIL_SIDE'],
     ['7','FAN_RELAY'],['8','OEM_SIDE_PROBE'],['9','OEM_CENTER_PROBE'],['10','PICKUP_RAW'],['11','GND_STAR'],['12','COIL_CENTER']
   ]},
   {ref:'U1L',value:'ESP32 DEVKITC LEFT 1x19',kind:'single',x:12,y:8,pitch:2.54,pins:espLeft},
   {ref:'U1R',value:'ESP32 DEVKITC RIGHT 1x19',kind:'single',x:37.4,y:8,pitch:2.54,pins:espRight},
-  {ref:'FMAIN',value:'5A',kind:'axial',x:25,y:125,pins:[['1','IGN_12V'],['2','VIN_FUSED']]},
-  {ref:'DREV',value:'SB560',kind:'axial',x:39,y:125,pins:[['A','VIN_FUSED'],['K','VIN_PROT']]},
-  {ref:'TVS_IN',value:'SMBJ33A/P6KE33A',kind:'axial',x:53,y:118,pins:[['A','GND_STAR'],['K','VIN_PROT']]},
-  {ref:'L_IN',value:'47uH 5A',kind:'axial',x:53,y:125,pins:[['1','VIN_PROT'],['2','VIN_FILT']]},
-  {ref:'C_IN',value:'470uF 35V',kind:'axial',x:67,y:118,pins:[['-','GND_STAR'],['+','VIN_FILT']]},
-  {ref:'FLOGIC',value:'1A',kind:'axial',x:67,y:125,pins:[['1','VIN_FILT'],['2','VIN_LOGIC']]},
-  {ref:'UBUCK',value:'MP1584 MODULE',kind:'quad',x:82,y:122,pins:[['1','VIN_LOGIC'],['2','GND_STAR'],['3','5V_LOGIC'],['4','GND_LOGIC']]},
-  {ref:'FHV',value:'3A',kind:'axial',x:105,y:125,pins:[['1','VIN_FILT'],['2','VIN_HV_FUSED']]},
-  {ref:'SW1',value:'SERVICE HV',kind:'axial',x:120,y:125,pins:[['1','VIN_HV_FUSED'],['2','VIN_HV']]},
-  {ref:'NT1',value:'GROUND STAR LINK',kind:'axial',x:12,y:125,pins:[['1','GND_STAR'],['2','GND_POWER']]},
+  {ref:'FMAIN',value:'5A',kind:'axial',x:25,y:155,pins:[['1','IGN_12V'],['2','VIN_FUSED']]},
+  {ref:'DREV',value:'SB560',kind:'axial',x:39,y:155,pins:[['A','VIN_FUSED'],['K','VIN_PROT']]},
+  {ref:'TVS_IN',value:'SMBJ33A/P6KE33A',kind:'axial',x:53,y:148,pins:[['A','GND_STAR'],['K','VIN_PROT']]},
+  {ref:'L_IN',value:'47uH 5A',kind:'axial',x:53,y:155,pins:[['1','VIN_PROT'],['2','VIN_FILT']]},
+  {ref:'C_IN',value:'470uF 35V',kind:'axial',x:67,y:148,pins:[['-','GND_STAR'],['+','VIN_FILT']]},
+  {ref:'FLOGIC',value:'1A',kind:'axial',x:67,y:155,pins:[['1','VIN_FILT'],['2','VIN_LOGIC']]},
+  {ref:'UBUCK',value:'MP1584 MODULE',kind:'quad',x:82,y:152,pins:[['1','VIN_LOGIC'],['2','GND_LOGIC'],['3','5V_LOGIC'],['4','GND_LOGIC']]},
+  {ref:'FHV',value:'3A',kind:'axial',x:105,y:155,pins:[['1','VIN_FILT'],['2','VIN_HV_FUSED']]},
+  {ref:'SW1',value:'SERVICE HV',kind:'axial',x:120,y:155,pins:[['1','VIN_HV_FUSED'],['2','VIN_HV']]},
+  {ref:'NT1',value:'GROUND STAR LINK',kind:'axial',x:12,y:155,pins:[['1','GND_STAR'],['2','GND_POWER']]},
+  {ref:'NT2',value:'LOGIC STAR LINK',kind:'axial',x:20,y:145,pins:[['1','GND_STAR'],['2','GND_LOGIC']]},
 
   {ref:'U2',value:'LM339N',kind:'dip',x:72,y:12,pitch:2.54,row:7.62,pins:[
     ['1','FAULT_N'],['2','PICKUP_OC'],['3','5V_LOGIC'],['4','PICKUP_ZERO'],['5','PICKUP_SENSE'],
-    ['6','ISENSE'],['7','IREF'],['8','HV_C_FB'],['9','VOV_REF'],['10','HV_S_FB'],['11','VOV_REF'],
+    ['6','ISENSE_FILTERED'],['7','IREF'],['8','HV_C_FB'],['9','VOV_REF'],['10','HV_S_FB'],['11','VOV_REF'],
     ['12','GND_LOGIC'],['13','FAULT_N'],['14','FAULT_N']
   ]},
   {ref:'U3',value:'PC817 CENTER',kind:'quad',x:70,y:45,pins:[['1','OEM_C_LED_A'],['2','GND_POWER'],['3','GND_LOGIC'],['4','OEM_CENTER']]},
@@ -50,8 +51,21 @@ const components = [
   {ref:'JTPS',value:'TPS SELECT 2x3',kind:'dual',x:55,y:45,pitch:2.54,row:7.62,pins:[
     ['1','TPS_A'],['2','TPS_REF_RAW'],['3','TPS_B'],['4','TPS_B'],['5','TPS_SIG_RAW'],['6','TPS_A']
   ]},
-  {ref:'QFAN',value:'BC337-40',kind:'triple',x:55,y:57,pins:[['C','FAN_RELAY'],['B','FAN_BASE'],['E','GND_LOGIC']]},
+  {ref:'QFAN',value:'BC337-40',kind:'triple',x:55,y:57,pins:[['C','FAN_RELAY'],['B','FAN_BASE'],['E','GND_POWER']]},
   {ref:'DFAN',value:'1N4007',kind:'axial',x:66,y:57,pins:[['A','FAN_RELAY'],['K','VIN_PROT']]},
+  {ref:'QSTROBE',value:'FQP30N06L / IRLZ44N',kind:'triple',x:5,y:60,pins:[['G','STROBE_GATE'],['D','STROBE_NEG'],['S','GND_POWER']]},
+  {ref:'JSTROBE',value:'LED STROBE 5V 1x2',kind:'header1x2',x:5,y:70,pins:[['1','5V_LOGIC'],['2','STROBE_NEG']]},
+  {ref:'JAUDIO',value:'AUDIO EXPANSION 1x2',kind:'header1x2',x:5,y:80,pins:[['1','VIN_PROT'],['2','GND_STAR']]},
+  {ref:'JUART',value:'UART SERVICE 1x4',kind:'header1x4',x:20,y:70,pins:[['1','5V_LOGIC'],['2','GND_LOGIC'],['3','UART_TX'],['4','UART_RX']]},
+  {ref:'JBENCH',value:'BENCH LOOP ONLY 1x2',kind:'header1x2',x:20,y:82,pins:[['1','GATE_C'],['2','BENCH_LOOP']]},
+
+  {ref:'DBAT_BAT',value:'BAT54S / DUAL SCHOTTKY',kind:'triple',x:5,y:95,pins:[['1','GND_LOGIC'],['2','3V3'],['3','VBAT_SENSE']]},
+  {ref:'DBAT_TEMP',value:'BAT54S / DUAL SCHOTTKY',kind:'triple',x:5,y:102,pins:[['1','GND_LOGIC'],['2','3V3'],['3','TEMP_SENSE']]},
+  {ref:'DBAT_TPS',value:'BAT54S TPS',kind:'triple',x:5,y:109,pins:[['1','GND_LOGIC'],['2','3V3'],['3','TPS_SIG_DIV']]},
+  {ref:'DBAT_TREF',value:'BAT54S TPS REF',kind:'triple',x:5,y:116,pins:[['1','GND_LOGIC'],['2','3V3'],['3','TPS_REF_DIV']]},
+  {ref:'DBAT_PICK',value:'BAT54S PICKUP',kind:'triple',x:5,y:123,pins:[['1','GND_LOGIC'],['2','5V_LOGIC'],['3','PICKUP_SENSE']]},
+  {ref:'DBAT_HVC',value:'BAT54S HV CENTER',kind:'triple',x:5,y:130,pins:[['1','GND_LOGIC'],['2','3V3'],['3','HV_C_ADC']]},
+  {ref:'DBAT_HVS',value:'BAT54S HV SIDE',kind:'triple',x:5,y:137,pins:[['1','GND_LOGIC'],['2','3V3'],['3','HV_S_ADC']]},
 
   {ref:'U5',value:'TC4427A DIP8',kind:'dip',x:115,y:65,pitch:2.54,row:7.62,pins:[
     ['1','NC'],['2','PWM_A_IN'],['3','GND_POWER'],['4','PWM_B_IN'],['5','DRV_B'],['6','VIN_HV'],['7','DRV_A'],['8','NC']
@@ -59,6 +73,8 @@ const components = [
   {ref:'QHV1',value:'IRF3205',kind:'triple',x:132,y:64,pins:[['G','GATE_Q1'],['D','LV_A'],['S','ISENSE']]},
   {ref:'QHV2',value:'IRF3205',kind:'triple',x:132,y:81,pins:[['G','GATE_Q2'],['D','LV_B'],['S','ISENSE']]},
   {ref:'RSENSE',value:'0.05R 5W',kind:'axial',x:120,y:94,pins:[['1','ISENSE'],['2','GND_POWER']]},
+  {ref:'DTVS1',value:'1.5KE33A',kind:'axial',x:130,y:115,pins:[['A','ISENSE'],['K','LV_A']]},
+  {ref:'DTVS2',value:'1.5KE33A',kind:'axial',x:130,y:122,pins:[['A','ISENSE'],['K','LV_B']]},
   {ref:'T1',value:'EE35 UNIVERSAL GRID 2.54mm',kind:'transformer_ee35',x:150,y:58,pins:[
     ['1','LV_A'],['2','VIN_HV'],['3','LV_B'],['4','HV_AC1'],['5','HV_AC2']
   ]},
@@ -68,56 +84,67 @@ const components = [
   {ref:'DREC4',value:'UF4007',kind:'axial',x:178,y:73,pins:[['A','GND_POWER'],['K','HV_AC2']]},
   {ref:'DCH_C',value:'UF4007',kind:'axial',x:192,y:52,pins:[['A','BRIDGE_PLUS'],['K','HV_CENTER']]},
   {ref:'DCH_S',value:'UF4007',kind:'axial',x:192,y:73,pins:[['A','BRIDGE_PLUS'],['K','HV_SIDE']]},
-  {ref:'C_CENTER',value:'1uF 630V MKP 9x4 HOLE',kind:'hv_cap_9x4',x:196,y:36,pins:[['1','HV_CENTER'],['2','COIL_CENTER']]},
-  {ref:'C_SIDE',value:'1uF 630V MKP 9x4 HOLE',kind:'hv_cap_9x4',x:196,y:82,pins:[['1','HV_SIDE'],['2','COIL_SIDE']]},
-  {ref:'SCR1',value:'BT151-800R',kind:'triple',x:182,y:103,pins:[['K','GND_POWER'],['A','HV_CENTER'],['G','SCR_GATE_C']]},
-  {ref:'SCR2',value:'BT151-800R',kind:'triple',x:203,y:103,pins:[['K','GND_POWER'],['A','HV_SIDE'],['G','SCR_GATE_S']]},
-  {ref:'QNC',value:'BC547B',kind:'triple',x:145,y:103,pins:[['C','QPC_BASE_R'],['B','GATE_C_BASE'],['E','GND_LOGIC']]},
-  {ref:'QPC',value:'BC557B',kind:'triple',x:158,y:103,pins:[['C','SCR_GATE_C_R'],['B','QPC_BASE'],['E','VIN_HV']]},
-  {ref:'QNS',value:'BC547B',kind:'triple',x:145,y:112,pins:[['C','QPS_BASE_R'],['B','GATE_S_BASE'],['E','GND_LOGIC']]},
-  {ref:'QPS',value:'BC557B',kind:'triple',x:158,y:112,pins:[['C','SCR_GATE_S_R'],['B','QPS_BASE'],['E','VIN_HV']]}
+  {ref:'C_CENTER',value:'1uF 630V MKP 9x4 HOLE',kind:'hv_cap_9x4',x:148,y:38,pins:[['1','HV_CENTER'],['2','COIL_CENTER']]},
+  {ref:'C_SIDE',value:'1uF 630V MKP 9x4 HOLE',kind:'hv_cap_9x4',x:196,y:92,pins:[['1','HV_SIDE'],['2','COIL_SIDE']]},
+  {ref:'SCR1',value:'BT151-800R',kind:'triple',x:182,y:133,pins:[['K','GND_POWER'],['A','HV_CENTER'],['G','SCR_GATE_C']]},
+  {ref:'SCR2',value:'BT151-800R',kind:'triple',x:203,y:133,pins:[['K','GND_POWER'],['A','HV_SIDE'],['G','SCR_GATE_S']]},
+  {ref:'QNC',value:'BC547B',kind:'triple',x:145,y:133,pins:[['C','QPC_BASE_R'],['B','GATE_C_BASE'],['E','GND_POWER']]},
+  {ref:'QPC',value:'BC557B',kind:'triple',x:158,y:133,pins:[['C','SCR_GATE_C_R'],['B','QPC_BASE'],['E','VIN_HV']]},
+  {ref:'QNS',value:'BC547B',kind:'triple',x:145,y:142,pins:[['C','QPS_BASE_R'],['B','GATE_S_BASE'],['E','GND_POWER']]},
+  {ref:'QPS',value:'BC557B',kind:'triple',x:158,y:142,pins:[['C','SCR_GATE_S_R'],['B','QPS_BASE'],['E','VIN_HV']]}
 ];
 
 const passives = [
   ['RVB1','100k','VIN_FILT','VBAT_SENSE'],['RVB2','22k','VBAT_SENSE','GND_LOGIC'],['RVB3','1k','VBAT_SENSE','VBAT_ADC'],
   ['RT1','4.7k','5V_LOGIC','TEMP_SENSOR'],['RT2','15k','TEMP_SENSOR','TEMP_SENSE'],['RT3','27k','TEMP_SENSE','GND_LOGIC'],['RT4','1k','TEMP_SENSE','TEMP_ADC'],
-  ['RTPS1','15k','TPS_SIG_RAW','TPS_SIG_DIV'],['RTPS2','27k','TPS_SIG_DIV','GND_LOGIC'],['RTPS3','1k','TPS_SIG_DIV','TPS_ADC'],
+  ['RTPS0','100R','5V_LOGIC','TPS_REF_RAW'],['RTPS1','15k','TPS_SIG_RAW','TPS_SIG_DIV'],['RTPS2','27k','TPS_SIG_DIV','GND_LOGIC'],['RTPS3','1k','TPS_SIG_DIV','TPS_ADC'],
   ['RTPS4','15k','TPS_REF_RAW','TPS_REF_DIV'],['RTPS5','27k','TPS_REF_DIV','GND_LOGIC'],['RTPS6','1k','TPS_REF_DIV','TPS_REF_ADC'],
   ['RPICK1','39k 0.5W','PICKUP_RAW','PICKUP_SENSE'],['RPICK2','10k','PICKUP_SENSE','VMID'],['RPICK3','10M','PICKUP_OC','PICKUP_SENSE'],['RPICK4','4.7k','3V3','PICKUP_OC'],['RPICK5','1k','PICKUP_OC','PICKUP_DIG'],
-  ['RVM1','10k','3V3','VMID'],['RVM2','10k','VMID','GND_LOGIC'],['RVZ1','15k','5V_LOGIC','PICKUP_ZERO'],['RVZ2','10k','PICKUP_ZERO','GND_LOGIC'],
+  ['RVM1','10k','5V_LOGIC','VMID'],['RVM2','10k','VMID','GND_LOGIC'],['RVZ1','15k','5V_LOGIC','PICKUP_ZERO'],['RVZ2','10k','PICKUP_ZERO','GND_LOGIC'],
   ['ROEMC1','12k 0.5W','COIL_CENTER','OEM_C_R1'],['ROEMC2','12k 0.5W','OEM_C_R1','OEM_C_R2'],['ROEMC3','12k 0.5W','OEM_C_R2','OEM_C_R3'],['ROEMC4','12k 0.5W','OEM_C_R3','OEM_C_LED_A'],
   ['ROEMS1','12k 0.5W','COIL_SIDE','OEM_S_R1'],['ROEMS2','12k 0.5W','OEM_S_R1','OEM_S_R2'],['ROEMS3','12k 0.5W','OEM_S_R2','OEM_S_R3'],['ROEMS4','12k 0.5W','OEM_S_R3','OEM_S_LED_A'],
   ['ROEMC5','4.7k','3V3','OEM_CENTER'],['ROEMS5','4.7k','3V3','OEM_SIDE'],
   ['RFAN1','4.7k','FAN_CTL','FAN_BASE'],['RFAN2','10k','FAN_BASE','GND_LOGIC'],
+  ['RSTR1','100R','STROBE','STROBE_GATE'],['RSTR2','10k','STROBE_GATE','GND_POWER'],
+  ['CBAT','10nF','VBAT_SENSE','GND_LOGIC'],['CTEMP','10nF','TEMP_SENSE','GND_LOGIC'],
+  ['CTPSS','4.7nF','TPS_SIG_DIV','GND_LOGIC'],['CTPSR','4.7nF','TPS_REF_DIV','GND_LOGIC'],
+  ['CU2','100nF','5V_LOGIC','GND_LOGIC'],
   ['RPWMA','1k','PWM_A','PWM_A_IN'],['RPWMB','1k','PWM_B','PWM_B_IN'],['RGA','10R','DRV_A','GATE_Q1'],['RGB','10R','DRV_B','GATE_Q2'],
   ['RGPD1','10k','GATE_Q1','ISENSE'],['RGPD2','10k','GATE_Q2','ISENSE'],
-  ['RI1','120k','3V3','IREF'],['RI2','10k','IREF','GND_LOGIC'],['RIS','100R','ISENSE','ISENSE_ADC'],
-  ['RVOV1','120k','5V_LOGIC','VOV_REF'],['RVOV2','100k','VOV_REF','GND_LOGIC'],
+  ['RPWMPDA','10k','PWM_A_IN','GND_POWER'],['RPWMPDB','10k','PWM_B_IN','GND_POWER'],
+  ['CTC1','100nF','VIN_HV','GND_POWER'],['CTC2','10uF 25V','VIN_HV','GND_POWER'],
+  ['RI1','120k','3V3','IREF'],['RI2','10k','IREF','GND_LOGIC'],['RIS','100R','ISENSE','ISENSE_FILTERED'],
+  ['RVOV1','120k','5V_LOGIC','VOV_REF'],['RVOV2','100k','VOV_REF','GND_LOGIC'],['CVOV','10nF','VOV_REF','GND_LOGIC'],
+  ['RFAULT','4.7k','3V3','FAULT_N'],['DCLA','1N4148','PWM_A','FAULT_N'],['DCLB','1N4148','PWM_B','FAULT_N'],
+  ['RHVP1','100k','VIN_HV','HV_PRESENT'],['RHVP2','27k','HV_PRESENT','GND_POWER'],
   ['RGC1','4.7k','GATE_C','GATE_C_BASE'],['RGC2','2.2k','QPC_BASE_R','QPC_BASE'],['RGC3','10k','VIN_HV','QPC_BASE'],['RGC4','330R','SCR_GATE_C_R','SCR_GATE_C'],['RGC5','1k','SCR_GATE_C','GND_POWER'],
-  ['RGS1','4.7k','GATE_S','GATE_S_BASE'],['RGS2','2.2k','QPS_BASE_R','QPS_BASE'],['RGS3','10k','VIN_HV','QPS_BASE'],['RGS4','330R','SCR_GATE_S_R','SCR_GATE_S'],['RGS5','1k','SCR_GATE_S','GND_POWER']
+  ['RGC6','10k','GATE_C_BASE','GND_POWER'],
+  ['RGS1','4.7k','GATE_S','GATE_S_BASE'],['RGS2','2.2k','QPS_BASE_R','QPS_BASE'],['RGS3','10k','VIN_HV','QPS_BASE'],['RGS4','330R','SCR_GATE_S_R','SCR_GATE_S'],['RGS5','1k','SCR_GATE_S','GND_POWER'],
+  ['RGS6','10k','GATE_S_BASE','GND_POWER']
 ];
 
-let px=48, py=64;
+let px=40, py=68;
 for (const [ref,val,a,b] of passives) {
-  const power = /^(RPWM|RGA|RGB|RGPD|RI\d|RIS|RVOV)/.test(ref);
+  const power = /^(RPWM|RGA|RGB|RGPD|CTC|RI\d|RIS|RVOV|CVOV|RFAULT|DCL|RHVP)/.test(ref);
   const gate = /^(RGC|RGS)/.test(ref);
   if (power) {
     const n=components.filter(c=>c._group==='powerPassive').length;
     components.push({ref,value:val,kind:'axial',x:103+(n%4)*10,y:8+Math.floor(n/4)*7,pins:[['1',a],['2',b]],_group:'powerPassive'});
   } else if (gate) {
     const n=components.filter(c=>c._group==='gatePassive').length;
-    components.push({ref,value:val,kind:'axial',x:145+(n%5)*13,y:84+Math.floor(n/5)*7,pins:[['1',a],['2',b]],_group:'gatePassive'});
+    components.push({ref,value:val,kind:'axial',x:145+(n%5)*13,y:112+Math.floor(n/5)*7,pins:[['1',a],['2',b]],_group:'gatePassive'});
   } else {
     components.push({ref,value:val,kind:'axial',x:px,y:py,pins:[['1',a],['2',b]],_group:'logicPassive'});
-    px += 12; if (px > 84) { px=48; py+=7; }
+    px += 11; if (px > 88) { px=40; py+=7; }
   }
 }
 
 for (const bank of [['C','HV_CENTER','HV_C_FB','HV_C_ADC'],['S','HV_SIDE','HV_S_FB','HV_S_ADC']]) {
   const [suffix,hv,fb,adc]=bank; let previous=hv;
   for(let n=1;n<=4;n++) { const next=n===4?fb:`HV_${suffix}_DIV${n}`; components.push({ref:`RHV${suffix}${n}`,value:'270k 1%',kind:'axial',x:174+n*10,y:suffix==='C'?8:18,pins:[['1',previous],['2',next]]}); previous=next; }
-  components.push({ref:`RHV${suffix}5`,value:'8.2k 1%',kind:'axial',x:184,y:suffix==='C'?25:28,pins:[['1',fb],['2','GND_LOGIC']]});
+  components.push({ref:`RHV${suffix}5`,value:'8.2k 1%',kind:'axial',x:184,y:suffix==='C'?25:28,pins:[['1',fb],['2','GND_POWER']]});
   components.push({ref:`RHV${suffix}6`,value:'1k',kind:'axial',x:198,y:suffix==='C'?25:28,pins:[['1',fb],['2',adc]]});
+  components.push({ref:`CFB${suffix}`,value:'10nF',kind:'axial',x:160,y:suffix==='C'?25:32,pins:[['1',fb],['2','GND_POWER']]});
 }
 
 for (const [suffix,hv,coil,y] of [['C','HV_CENTER','COIL_CENTER',35],['S','HV_SIDE','COIL_SIDE',79]]) {
@@ -132,6 +159,10 @@ function footprint(c) {
   const pads=[];
   const addPad=(p,dx,dy,w=mm(2.6),h=mm(2.6),hole=mm(0.55))=>pads.push(`PAD~ELLIPSE~${x+dx}~${y+dy}~${w}~${h}~11~${p[1]}~${p[0]}~${hole}~~0~${gid()}`);
   if(c.kind==='single') c.pins.forEach((p,i)=>addPad(p,0,mm(i*2.54)));
+  else if(c.kind==='header1x2'||c.kind==='header1x4') {
+    c.pins.forEach((p,i)=>addPad(p,mm(i*2.54),0,mm(2),mm(2),mm(1)));
+    shapes.push(`TRACK~0.5~3~~${x-mm(1.27)} ${y-mm(1.27)} ${x+mm((c.pins.length-1)*2.54+1.27)} ${y-mm(1.27)} ${x+mm((c.pins.length-1)*2.54+1.27)} ${y+mm(1.27)} ${x-mm(1.27)} ${y+mm(1.27)} ${x-mm(1.27)} ${y-mm(1.27)}~${gid()}`);
+  }
   else if(c.kind==='header2x6') {
     c.pins.forEach((p,i)=>addPad(p,mm((i%6)*2.54),mm(i<6?0:2.54),mm(2),mm(2),mm(1)));
     shapes.push(`TRACK~0.5~3~~${x-mm(1.27)} ${y-mm(1.27)} ${x+mm(13.97)} ${y-mm(1.27)} ${x+mm(13.97)} ${y+mm(3.81)} ${x-mm(1.27)} ${y+mm(3.81)} ${x-mm(1.27)} ${y-mm(1.27)}~${gid()}`);
@@ -164,7 +195,7 @@ function footprint(c) {
 
 function board(variant) {
   id=1000;
-  const ox=100, oy=100, w=mm(230), h=mm(135);
+  const ox=100, oy=100, w=mm(230), h=mm(165);
   const shape=[];
   shape.push(`TRACK~1~10~~${ox} ${oy} ${ox+w} ${oy} ${ox+w} ${oy+h} ${ox} ${oy+h} ${ox} ${oy}~${gid()}`);
   shape.push(`TEXT~L~${ox+10}~${oy+15}~1~0~none~3~~10~IGNITRA CDI R9 ESP32 ${variant} - PLACEMENT/NET TEMPLATE~~~${gid()}`);
@@ -172,7 +203,7 @@ function board(variant) {
   // Functional-zone boundaries and isolation-slot guides.
   shape.push(`TRACK~1~12~~${ox+mm(100)} ${oy} ${ox+mm(100)} ${oy+h}~${gid()}`);
   shape.push(`TRACK~1~12~~${ox+mm(145)} ${oy} ${ox+mm(145)} ${oy+h}~${gid()}`);
-  shape.push(`SOLIDREGION~10~~${ox+mm(143)} ${oy+mm(5)} ${ox+mm(145)} ${oy+mm(5)} ${ox+mm(145)} ${oy+mm(130)} ${ox+mm(143)} ${oy+mm(130)}~npth~${gid()}`);
+  shape.push(`SOLIDREGION~10~~${ox+mm(143)} ${oy+mm(5)} ${ox+mm(145)} ${oy+mm(5)} ${ox+mm(145)} ${oy+mm(160)} ${ox+mm(143)} ${oy+mm(160)}~npth~${gid()}`);
   for(const c of components) shape.push(footprint(c));
   return {
     head:'3~1.11.3~Author`OpenAI Codex`Project`IGNITRA CDI R9 ESP32`',
@@ -240,7 +271,7 @@ const svgParts = components.map(c => {
     c.kind === 'dip' || c.kind === 'dual' ? Math.max(8, c.pins.length * 1.4) : 6;
   return `<g><rect x="${c.x}" y="${c.y}" width="${width}" height="${height}" rx="0.8" fill="${zoneColor(c.x)}" stroke="#d8e6ef" stroke-width="0.35"/><text x="${c.x+0.8}" y="${c.y+2.7}" font-size="2.3" fill="#ffffff">${c.ref}</text></g>`;
 }).join('');
-const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 230 135" width="2300" height="1350"><rect width="230" height="135" fill="#0a0b0d"/><rect x="0" y="0" width="100" height="135" fill="#0c1c2b"/><rect x="100" y="0" width="45" height="135" fill="#2a210e"/><rect x="145" y="0" width="85" height="135" fill="#2b0e13"/><path d="M100 0V135M145 0V135" stroke="#ffffff" stroke-width="0.4" stroke-dasharray="2 1"/><text x="2" y="133" font-size="3" fill="#6ee7ff">LOGIC</text><text x="102" y="133" font-size="3" fill="#ffc857">POWER 12V</text><text x="147" y="133" font-size="3" fill="#ff6b6b">HV 345V</text>${svgParts}</svg>`;
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 230 165" width="2300" height="1650"><rect width="230" height="165" fill="#0a0b0d"/><rect x="0" y="0" width="100" height="165" fill="#0c1c2b"/><rect x="100" y="0" width="45" height="165" fill="#2a210e"/><rect x="145" y="0" width="85" height="165" fill="#2b0e13"/><path d="M100 0V165M145 0V165" stroke="#ffffff" stroke-width="0.4" stroke-dasharray="2 1"/><text x="2" y="163" font-size="3" fill="#6ee7ff">LOGIC</text><text x="102" y="163" font-size="3" fill="#ffc857">POWER 12V</text><text x="147" y="163" font-size="3" fill="#ff6b6b">HV 345V</text>${svgParts}</svg>`;
 fs.writeFileSync(path.join(out,'PLACEMENT_PREVIEW.svg'),svg);
 
 console.log(`Generated ${components.length} footprints into ${out}`);
